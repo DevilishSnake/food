c725485b55ab8792beb28e173e2bc7bbe86d3d3d
DevilishSnake <joanleonelcortes@hotmail.com>
2022-10-22 18:42:17 -0300

    Recipe model modified

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,34 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
-    name: {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    title: {
       type: DataTypes.STRING,
+      defaultValue: "Default recipe title from DB",
       allowNull: false,
     },
+    resume: {
+      type: DataTypes.STRING,
+      defaultValue: "This is a Default resume of the database recipe",
+      allowNull: false
+    }, 
+    healthScore: {
+      type: DataTypes.INTEGER,
+      defaultValue: 50
+    }, 
+    steps: {
+      type: DataTypes.STRING,
+      defaultValue: "1- Default step number One and final"
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+      allowNull: false
+    }
   });
 };
